9d0c7a18c595082d1879a04b407b324fed171b49
Ansari Aamir <sabriaamir74@gmail.com>
2021-06-30 16:40:30 +0530

    Add files via upload

---
+++ frontend/src/products/components/GithubRibbon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './GithubRibbon.css'
+
+const GithubRibbon = () => {
+    return (
+        <a className="gitfork" href="https://github.com/aamir74/Ecommerce-WebApp"><img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_left_red_aa0000.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" /></a>    )
+}
+
+export default GithubRibbon
