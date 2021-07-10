5161289c3ed4e120ae735717763db8730925f749
Ansari Aamir <sabriaamir74@gmail.com>
2021-07-10 11:37:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -31,8 +31,7 @@ app.use((req, res, next) => {
 
 
 mongoose
-    .connect(`mongodb+srv://Aamir:Aamir8692@cluster0.st5bw.mongodb.net/ecommerce?retryWrites=true&w=majority`, { useNewUrlParser: true })
-    .then(() => {
+  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.st5bw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true })    .then(() => {
         app.listen(5000)
     })
     .catch(err => {
